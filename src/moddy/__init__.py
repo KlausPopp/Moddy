34cf095b68a4b9f3d4d6960af38e5249cf31bfcb
klaus <klauspopp@gmx.de>
2017-03-26 21:32:34 +0200

    Set version to 1.2.0

--- src/moddy/__init__.py
+++ src/moddy/__init__.py
@@ -17,7 +17,7 @@ Moddy was written to analyze complex systems in the concept phase to validate th
 
 
 
-version = (1, 1, 0)  
+version = (1, 2, 0)  
 VERSION = '%d.%d.%d' % version
 
 AUTHOR_NAME = 'Klaus Popp'
