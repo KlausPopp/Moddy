09219b54bb3a68542e42abbfc6c1a536db807ac1
klaus <klauspopp@gmx.de>
2018-12-19 19:59:22 +0100

    Moddy 1.5.0
    Add Commonly used status box appearance colors bcXXX

--- src/moddy/__init__.py
+++ src/moddy/__init__.py
@@ -4,7 +4,7 @@
 # Purpose: package definition file
 # Created: 2016-12-26
 # License: LGPLv3
-# Copyright (C) 2016-2017 Klaus Popp (klauspopp@gmx.de)
+# Copyright (C) 2016-2019 Klaus Popp (klauspopp@gmx.de)
 
 """
 Moddy is a discrete event simulator to model and analyze the timing behavior of systems consisting of 
@@ -17,16 +17,29 @@ Moddy was written to analyze complex systems in the concept phase to validate th
 
 
 
-version = (1, 4, 2)  
+version = (1, 5, 0)  
 VERSION = '%d.%d.%d' % version
 
 AUTHOR_NAME = 'Klaus Popp'
 AUTHOR_EMAIL = 'klauspopp@gmx.de'
 CYEAR = '2017'
 
+# Time factors
 ns = 1E-9
 us = 1E-6
 ms = 1E-3
+
+# Commonly used status box appearance colors
+bcWhiteOnGreen = {'boxStrokeColor':'black', 'boxFillColor':'green', 'textColor':'white'}
+bcWhiteOnRed = {'boxStrokeColor':'black', 'boxFillColor':'red', 'textColor':'white'}
+bcWhiteOnBlue = {'boxStrokeColor':'blue', 'boxFillColor':'blue', 'textColor':'white'}
+bcWhiteOnBrown = {'boxStrokeColor':'brown', 'boxFillColor':'brown', 'textColor':'white'}
+bcWhiteOnBlack = {'boxStrokeColor':'black', 'boxFillColor':'black', 'textColor':'white'}
+bcBlackOnPink = {'boxStrokeColor':'pink', 'boxFillColor':'pink', 'textColor':'black'}
+bcBlackOnGrey = {'boxStrokeColor':'grey', 'boxFillColor':'grey', 'textColor':'black'}
+bcWhiteOnGrey = {'boxStrokeColor':'grey', 'boxFillColor':'grey', 'textColor':'white'}
+bcBlackOnWhite = {'boxStrokeColor':'black', 'boxFillColor':'white', 'textColor':'black'}
+
 
 # import moddy global api
 from moddy.simulator import sim,simPart
