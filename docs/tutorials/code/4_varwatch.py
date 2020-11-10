145bb541c75861facf3c3b61cc2160ef2fa44211
popples <klauspopp@gmx.de>
2020-11-10 21:10:13 +0100

    Refactor varwatch demo and fix sim_var_watch

--- docs/tutorials/code/4_varwatch.py
+++ docs/tutorials/code/4_varwatch.py
@@ -1,43 +1,46 @@
-'''
+"""
 @author: klauspopp@gmx.de
-'''
+"""
 
 import moddy
 
-def varChangerProg(self):
+
+def var_changer_prog(self):
     self.var1 = 0
     # self.var2 is created during execution
     while True:
         self.var1 = 1
         self.wait(2)
-        
+
         self.var1 = 2
         self.var2 = "ABC"
 
         self.wait(2)
         self.var2 = "DEF"
-        
-        self.busy(2,'WAIT')
+
+        self.busy(2, "WAIT")
         self.var1 = 3
-        
+
         self.wait(3)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     SIMU = moddy.Sim()
-    vc = moddy.VSimpleProg( sim=SIMU, obj_name="VC", target=varChangerProg )
+    VC = moddy.VSimpleProg(sim=SIMU, obj_name="VC", target=var_changer_prog)
 
-    var1watch = vc.new_var_watcher('var1', "0x%08x")
-    var2watch = vc.new_var_watcher('var2', "%s")
-        
+    VC.new_var_watcher("var1", "0x%08x")
+    VC.new_var_watcher("var2", "%s")
+
     SIMU.run(10)
-    moddy.moddyGenerateTraceTable( SIMU, 'output/4_varwatch.csv')
+    moddy.gen_trace_table(SIMU, "output/4_varwatch.csv")
 
-    moddy.moddyGenerateSequenceDiagram( sim=SIMU, 
-                                  fileName="output/4_varwatch.html", 
-                                  fmt="iaViewer", 
-                                  showPartsList=['VC'],
-                                  showVarList=['VC.var1', 'VC.var2'],
-                                  excludedElementList=['allTimers'], 
-                                  timePerDiv = 0.5, 
-                                  pixPerDiv = 30,
-                                  title="Variable Watcher Demo")    
+    moddy.gen_interactive_sequence_diagram(
+        sim=SIMU,
+        file_name="output/4_varwatch.html",
+        show_parts_list=["VC"],
+        show_var_list=["VC.var1", "VC.var2"],
+        excluded_element_list=["allTimers"],
+        time_per_div=0.5,
+        pix_per_div=30,
+        title="Variable Watcher Demo",
+    )
--- src/moddy/sim_var_watch.py
+++ src/moddy/sim_var_watch.py
@@ -1,4 +1,4 @@
-'''
+"""
 :mod:`sim_var_watch` -- Variable Watcher
 ========================================
 
@@ -6,13 +6,13 @@
    :synopsis: Variable Watcher
 .. moduleauthor:: Klaus Popp <klauspopp@gmx.de>
 
-'''
+"""
 from .sim_base import SimBaseElement, add_elem_to_list
 from .sim_trace import SimTraceEvent
 
 
 class SimVariableWatcher(SimBaseElement):
-    '''
+    """
     The VariableWatcher class watches a variable for changes.
     The variable is referenced by the moddy part and its variable name
     within the part.
@@ -21,39 +21,39 @@ class SimVariableWatcher(SimBaseElement):
     The class provides the check_value_changed() method. In moddy,
     the simulator should call this function
     after each event (or step) to see if the value has changed
-    '''
+    """
 
     def __init__(self, sim, part, var_name, format_string):
-        '''
+        """
         :param sim sim: simulator object
         :param simPart part: part which contains the variable
         :param str varName: Variable name as seen part scope
         :param str formatString: print format like string to format value
-        '''
+        """
         super().__init__(sim, part, var_name, "WatchedVar")
         self._var_name = var_name
         self._last_value = None
         self._format_string = format_string
 
     def current_value(self):
-        '''return current value of watched var'''
+        """return current value of watched var"""
         # pylint: disable=eval-used, bare-except
         try:
-            cur_val = eval('self.parent_obj.' + self._var_name)
-        except NameError:
+            cur_val = eval("self.parent_obj." + self._var_name)
+        except (NameError, AttributeError):
             cur_val = None
         return cur_val
 
     def __str__(self):
         cur_val = self.current_value()
         if cur_val is None:
-            ret_val = ''
+            ret_val = ""
         else:
             ret_val = self._format_string % (cur_val)
         return ret_val
 
     def check_value_changed(self):
-        '''
+        """
         Check if the variable value has changed
         :return: Changed, newVal
 
@@ -65,7 +65,7 @@ class SimVariableWatcher(SimBaseElement):
         (e.g. because the variable does not exist (anymore))
         the variables value is set to None (no exception is raised)
 
-        '''
+        """
         old_val = self._last_value
         cur_val = self.current_value()
         changed = False
@@ -77,16 +77,16 @@ class SimVariableWatcher(SimBaseElement):
         return (changed, cur_val)
 
     def var_name(self):
-        '''
+        """
         :return: Name of watched variable
-        '''
+        """
         return self._var_name
 
 
 class SimVarWatchManager:
-    '''
+    """
     Class that tracks all watched variables
-    '''
+    """
 
     def __init__(self, sim_tracing):
         self._sim_tracing = sim_tracing
@@ -94,43 +94,50 @@ class SimVarWatchManager:
         self._list_variable_watches = []  # list of watched variables
 
     def add_var_watcher(self, var_watcher):
-        '''Add watcher to watcher list'''
-        add_elem_to_list(self._list_variable_watches, var_watcher,
-                         "Simulator Watcher")
+        """Add watcher to watcher list"""
+        add_elem_to_list(
+            self._list_variable_watches, var_watcher, "Simulator Watcher"
+        )
 
     def watch_variables(self):
-        '''
+        """
         Check all registered variables for changes.
         Generate a trace event for all changed variables
-        '''
+        """
         for var_watcher in self._list_variable_watches:
             changed, _ = var_watcher.check_value_changed()
             if changed:
                 new_val_str = var_watcher.__str__()
-                trace_ev = SimTraceEvent(var_watcher.parent_obj,
-                                         var_watcher, new_val_str, 'VC')
+                trace_ev = SimTraceEvent(
+                    var_watcher.parent_obj, var_watcher, new_val_str, "VC"
+                )
                 self._sim_tracing.add_trace_event(trace_ev)
 
     def watch_variables_current_value(self):
-        '''
+        """
         Generate a trace event for all watched variables with their
         current value.
         Used at start of simulator to report the initial values
-        '''
+        """
         for var_watcher in self._list_variable_watches:
-            trace_ev = SimTraceEvent(var_watcher.parent_obj,
-                                     var_watcher, var_watcher.__str__(), 'VC')
+            trace_ev = SimTraceEvent(
+                var_watcher.parent_obj,
+                var_watcher,
+                var_watcher.__str__(),
+                "VC",
+            )
             self._sim_tracing.add_trace_event(trace_ev)
 
     def find_watched_variable_by_name(self, variable_hierarchy_name):
-        '''
+        """
         Find a watched variable by its hierarchy name
         :param str variable_hierarchy_name: e.g. "part1.variable"
         :return SimVariableWatcher: the found variable watcher
         :raises ValueError: if variable not found
-        '''
+        """
         for var_watcher in self._list_variable_watches:
             if var_watcher.hierarchy_name() == variable_hierarchy_name:
                 return var_watcher
-        raise ValueError("Watched Variable not found %s" %
-                         variable_hierarchy_name)
+        raise ValueError(
+            "Watched Variable not found %s" % variable_hierarchy_name
+        )
