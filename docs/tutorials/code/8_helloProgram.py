3af0e58c0f3328b04a3c17cb7506f4d3d22b802c
popples <klauspopp@gmx.de>
2020-11-13 21:17:35 +0100

    Refactor 8_helloProgram

--- docs/tutorials/code/8_helloProgram.py
+++ docs/tutorials/code/8_helloProgram.py
@@ -1,36 +1,37 @@
-'''
+"""
 @author: klauspopp@gmx.de
 
 The Moddy 1_hello demo modelled using Moddy sequential programs
-'''
+"""
 
-from moddy import *
+import moddy
 
-def bobProg(self: vSimpleProg):
+
+def bob_prog(self: moddy.VSimpleProg):
     # bob starts talking
     self.head.send("Hi Joe", 1)
-    
+
     while True:
-        msg = self.waitForMsg(None, self.head)
-        self.addAnnotation('got message ' + msg)
-        
-        self.busy( 1.4, 'Think')
-        
+        msg = self.wait_for_msg(None, self.head)
+        self.annotation("got message " + msg)
+
+        self.busy(1.4, "Think")
+
         if msg == "Hi, How are you?":
             reply = "How are you?"
         else:
             reply = "Hm?"
 
         self.head.send(reply, 1)
-        
-        
-def joeProg(self: vSimpleProg):
+
+
+def joe_prog(self: moddy.VSimpleProg):
     while True:
-        msg = self.waitForMsg(None, self.head)
-        self.addAnnotation('got message ' + msg)
-        
-        self.busy( 2, 'Think')
-        
+        msg = self.wait_for_msg(None, self.head)
+        self.annotation("got message " + msg)
+
+        self.busy(2, "Think")
+
         if msg == "Hi Joe":
             reply = "Hi, How are you?"
         elif msg == "How are you?":
@@ -38,30 +39,33 @@ def joeProg(self: vSimpleProg):
         else:
             reply = "Hm?"
 
-
         self.head.send(reply, 1.5)
 
 
-if __name__ == '__main__':
-    SIMU = sim()
-    
-    vSimpleProg( sim=SIMU, objName="Bob", target=bobProg, elems={ 'QueuingIO': 'head' } )
-    vSimpleProg( sim=SIMU, objName="Joe", target=joeProg, elems={ 'QueuingIO': 'head' } )
-    
-    SIMU.smartBind([ ['Bob.head', 'Joe.head'] ])
+if __name__ == "__main__":
+    SIMU = moddy.Sim()
+
+    moddy.VSimpleProg(
+        sim=SIMU, obj_name="Bob", target=bob_prog, elems={"QueuingIO": "head"}
+    )
+    moddy.VSimpleProg(
+        sim=SIMU, obj_name="Joe", target=joe_prog, elems={"QueuingIO": "head"}
+    )
+
+    SIMU.smart_bind([["Bob.head", "Joe.head"]])
 
     # let simulator run
-    SIMU.run(stopTime=12.0)
-    
+    SIMU.run(stop_time=12.0)
+
     # Output sequence diagram
-    moddyGenerateSequenceDiagram( sim=SIMU, 
-                                  fileName="output/8_helloProgram.html", 
-                                  fmt="iaViewer", 
-                                  showPartsList=['Bob', 'Joe'], 
-                                  timePerDiv = 1.0, 
-                                  pixPerDiv = 30,
-                                  title = "Hello Program Demo")    
+    moddy.gen_interactive_sequence_diagram(
+        sim=SIMU,
+        file_name="output/8_helloProgram.html",
+        show_parts_list=["Bob", "Joe"],
+        time_per_div=1.0,
+        pix_per_div=30,
+        title="Hello Program Demo",
+    )
 
     # Output model structure graph
-    moddyGenerateStructureGraph(SIMU, 'output/8_hello_structure.svg')
-
+    moddy.gen_dot_structure_graph(SIMU, "output/8_hello_structure.svg")
