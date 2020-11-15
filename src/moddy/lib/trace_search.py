5a232ebb4c7930146c3bd23877beb731723131b1
popples <klauspopp@gmx.de>
2020-11-15 17:36:32 +0100

    refactor lib/trace_search

---
+++ src/moddy/lib/trace_search.py
@@ -0,0 +1,177 @@
+"""
+Created on 15.09.2019
+
+@author: klauspopp@gmx.de
+"""
+# import moddy
+import fnmatch
+
+
+class TraceSearch(object):
+    """
+    Class to search moddy traced events
+    """
+
+    def __init__(self, sim):
+        self.sim = sim
+        self.traced_events = sim.tracing.traced_events()
+        self.curIdx = 0
+
+    def find_ann(self, part, text_pat, start_idx=None):
+        """
+        find next Annotation
+        :param string textPat: text pattern with wildcards. \
+            If None, matches any text
+        other parameters and return, see findEvent
+        """
+        return self.find_event(
+            part, start_idx, self.tv_str_match, ("ANN", text_pat)
+        )
+
+    def find_ass_fail(self, part, text_pat, start_idx=None):
+        """
+        find next Assertion Failure
+        :param string textPat: text pattern with wildcards. \
+            If None, matches any text
+        other parameters and return, see findEvent
+        """
+        return self.find_event(
+            part, start_idx, self.tv_str_match, ("ASSFAIL", text_pat)
+        )
+
+    def find_sta(self, part, text_pat, start_idx=None):
+        """
+        find next Status event
+        :param string textPat: text pattern with wildcards. \
+            If None, matches any text
+        other parameters and return, see findEvent
+        """
+        return self.find_event(
+            part, start_idx, self.tv_str_match, ("STA", text_pat)
+        )
+
+    def find_rcv_msg(self, part, textPat, start_idx=None):
+        """
+        find next received message by text pattern on message 
+        string representation
+        "part" is the part receiving the message
+        :param string textPat: message text with wildcards. \
+            If None, matches any
+        other parameters and return, see findEvent
+        """
+        return self.find_event(
+            part,
+            start_idx,
+            self.msg_match,
+            ("<MSG", textPat),
+            part_matcher=self.sub_part_parent_match,
+        )
+
+    def find_snd_msg(self, part, text_pat, start_idx=None):
+        """
+        find next sent message by text pattern on message string representation
+        "part" is the part sending the message
+        :param string textPat: message text with wildcards.
+        If None, matches any other parameters and return, see findEvent
+        """
+        return self.find_event(
+            part,
+            start_idx,
+            self.msg_match,
+            (">MSG", text_pat),
+            part_matcher=self.sub_part_parent_match,
+        )
+
+    def find_vc(self, var_watcher, text_pat, start_idx=None):
+        """
+        find next value change event by text pattern
+        "varChanger" must be the varWatcher instance
+        (hierarchy name not supported!)
+        :param string textPat: message text with wildcards.
+        If None, matches any other parameters and return, see findEvent
+        """
+        return self.find_event(
+            var_watcher,
+            start_idx,
+            self.tv_str_match,
+            ("VC", text_pat),
+            part_matcher=self.sub_part_match,
+        )
+
+    def tv_str_match(self, para, te):
+        m_type, text_pat = para
+        rv = False
+        if te.action == m_type:
+            if self.wildcard_match(te.trans_val.__str__(), text_pat):
+                rv = True
+        return rv
+
+    def msg_match(self, para, te):
+        m_type, text_pat = para
+        rv = False
+        if te.action == m_type:
+            if self.wildcard_match(te.trans_val.msg_text(), text_pat):
+                rv = True
+        return rv
+
+    def find_event(
+        self, part, start_idx, match_func, match_func_para, part_matcher=None
+    ):
+        """
+        find next traced event
+        :param part: part hierarchy name or instance. If None, match any part
+            This is passed to partMatcher. So, depending on partMatcher, 
+            it can be a part, a part hierarchy name, or a subpart
+        :param startIdx: index in tracedEvents to start with \
+            (use curIdx if None)
+        :param partMatcher: function to call to check if trace event \
+            matches part
+            if None, use partMatch(te,p)
+        :return: idx, te=the index and found event or None
+        :raises ValueError: if part name not found
+        """
+        if part_matcher is None:
+            part_matcher = self.part_match
+        idx = start_idx if start_idx is not None else self.curIdx
+        part = self.part_translate(part)
+
+        rv = None
+        for idx in range(idx, len(self.traced_events)):
+            te = self.traced_events[idx]
+            # print("COMPARING te %d: %s" % (idx, te))
+            # if te.part == part or self.subPartMatch(part, te):
+            if part_matcher(te, part):
+                if match_func(match_func_para, te):
+                    rv = (idx, te)
+                    break
+        self.curIdx = idx + 1
+        return rv
+
+    @staticmethod
+    def part_match(te, p):
+        return te.part == p
+
+    @staticmethod
+    def sub_part_parent_match(te, p):
+        subpart = te.sub_obj
+        if subpart is None:
+            return False
+
+        return subpart.parent_obj == p
+
+    @staticmethod
+    def sub_part_match(te, p):
+        return te.sub_obj == p
+
+    def part_translate(self, part):
+        """
+        :param part: part hierarchy name or instance. If None, match any part
+        :return part instance
+        :raises ValueError: if part name not found
+        """
+        if type(part) is str:
+            part = self.sim.parts_mgr.find_part_by_name(part)
+        return part
+
+    def wildcard_match(self, txt, pattern):
+        return pattern is None or fnmatch.fnmatch(txt, pattern)
--- tests/lib/testTraceSearch.py
+++ tests/lib/testTraceSearch.py
@@ -1,119 +1,130 @@
-'''
+"""
 Created on 16.09.2019
 
 @author: klauspopp@gmx.de
-'''
+"""
 import unittest
-from moddy import *
-from moddy.lib.traceSearch import TraceSearch
+import moddy
+from moddy.lib.trace_search import TraceSearch
+
 
 class TestTraceSearch(unittest.TestCase):
-    
     def setUp(self):
         unittest.TestCase.setUp(self)
-        self.simu = simu = sim()
-        
-        vSimpleProg( sim=simu, objName="Bob", target=self.bobProg, elems={ 'QueuingIO': 'head' } )
-        vSimpleProg( sim=simu, objName="Joe", target=self.joeProg, elems={ 'QueuingIO': 'head' } )
-        
-        simu.smartBind([ ['Bob.head', 'Joe.head'] ])
-    
+        self.simu = simu = moddy.Sim()
+
+        moddy.VSimpleProg(
+            sim=simu,
+            obj_name="Bob",
+            target=self.bob_prog,
+            elems={"QueuingIO": "head"},
+        )
+        moddy.VSimpleProg(
+            sim=simu,
+            obj_name="Joe",
+            target=self.joe_prog,
+            elems={"QueuingIO": "head"},
+        )
+
+        simu.smart_bind([["Bob.head", "Joe.head"]])
+
         # let simulator run
-        simu.run(stopTime=12.0, enableTracePrinting=False, stopOnAssertionFailure=False)
-        
+        simu.run(
+            stop_time=12.0,
+            enable_trace_printing=False,
+            stop_on_assertion_failure=False,
+        )
+
         self.ts = TraceSearch(simu)
         idx = 0
-        for te in self.ts.tracedEvents:
-            print("#%d: %s" % (idx,te))
-            idx+=1
-            
-    def testfindAnn(self):
+        for te in self.ts.traced_events:
+            print("#%d: %s" % (idx, te))
+            idx += 1
+
+    def test_find_ann(self):
         ts = self.ts
 
-        # find any ANN        
-        rv = ts.findAnn('Bob', None, 0)
+        # find any ANN
+        rv = ts.find_ann("Bob", None, 0)
         self.assertEqual(rv[0], 16)
-        self.assertEqual(rv[1].transVal, "got message Hi, How are you?")
-         
-        rv = ts.findAnn('Bob', None)
+        self.assertEqual(rv[1].trans_val, "got message Hi, How are you?")
+
+        rv = ts.find_ann("Bob", None)
         self.assertEqual(rv[0], 34)
-        self.assertEqual(rv[1].transVal, "got message Fine")
-        
+        self.assertEqual(rv[1].trans_val, "got message Fine")
+
         # find with wildcard match
-        rv = ts.findAnn('Bob', "*Fine", 15)
+        rv = ts.find_ann("Bob", "*Fine", 15)
         self.assertEqual(rv[0], 34)
-        self.assertEqual(rv[1].transVal, "got message Fine")
-        
-    def testfindSta(self):
+        self.assertEqual(rv[1].trans_val, "got message Fine")
+
+    def test_find_sta(self):
         ts = self.ts
 
-        rv = ts.findSta('Joe', "Think", 0)
+        rv = ts.find_sta("Joe", "Think", 0)
         self.assertEqual(rv[0], 9)
-        rv = ts.findSta('Joe', "")
+        rv = ts.find_sta("Joe", "")
         self.assertEqual(rv[0], 13)
-    
-    def testfindRcvMsg(self):
+
+    def test_find_rcv_msg(self):
         ts = self.ts
 
-        rv = ts.findRcvMsg('Joe', None, 0)
+        rv = ts.find_rcv_msg("Joe", None, 0)
         self.assertEqual(rv[0], 5)
 
-    def testfindSndMsg(self):
+    def test_find_snd_msg(self):
         ts = self.ts
 
-        rv = ts.findSndMsg('Bob', "Hi*", 0)
+        rv = ts.find_snd_msg("Bob", "Hi*", 0)
         self.assertEqual(rv[0], 1)
 
-        rv = ts.findSndMsg('Bob', "How*")
+        rv = ts.find_snd_msg("Bob", "How*")
         self.assertEqual(rv[0], 21)
 
-    def testfindAssFail(self):
+    def test_find_ass_fail(self):
         ts = self.ts
 
-        rv = ts.findAssFail('Bob', "Unknown*", 0)
+        rv = ts.find_ass_fail("Bob", "Unknown*", 0)
         self.assertEqual(rv[0], 39)
-        
-        rv = ts.findAssFail('Joe', None, 0)
+
+        rv = ts.find_ass_fail("Joe", None, 0)
         self.assertEqual(rv, None)
-        
 
     @staticmethod
-    def bobProg(self: vSimpleProg):
+    def bob_prog(self: moddy.VSimpleProg):
         # bob starts talking
         self.head.send("Hi Joe", 1)
-        
+
         while True:
-            msg = self.waitForMsg(None, self.head)
-            self.addAnnotation('got message ' + msg)
-            
-            self.busy( 1.4, 'Think')
-            
+            msg = self.wait_for_msg(None, self.head)
+            self.annotation("got message " + msg)
+
+            self.busy(1.4, "Think")
+
             if msg == "Hi, How are you?":
                 reply = "How are you?"
             else:
-                self.assertionFailed("Unknown msg %s" % msg)
-    
+                self.assertion_failed("Unknown msg %s" % msg)
+
             self.head.send(reply, 1)
-            
-            
+
     @staticmethod
-    def joeProg(self: vSimpleProg):
+    def joe_prog(self: moddy.VSimpleProg):
         while True:
-            msg = self.waitForMsg(None, self.head)
-            self.addAnnotation('got message ' + msg)
-            
-            self.busy( 2, 'Think')
-            
+            msg = self.wait_for_msg(None, self.head)
+            self.annotation("got message " + msg)
+
+            self.busy(2, "Think")
+
             if msg == "Hi Joe":
                 reply = "Hi, How are you?"
             elif msg == "How are you?":
                 reply = "Fine"
             else:
                 reply = "Hm?"
-    
-    
+
             self.head.send(reply, 1.5)
-    
-    
-    
-    
+
+
+if __name__ == "__main__":
+    unittest.main()
