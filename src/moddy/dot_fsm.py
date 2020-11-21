56bdbde963ed73e9f34ab5c817414a567b61be64
popples <klauspopp@gmx.de>
2020-11-21 17:20:57 +0100

    bug fix in dot_fsm.py

--- src/moddy/dot_fsm.py
+++ src/moddy/dot_fsm.py
@@ -1,4 +1,4 @@
-'''
+"""
 :mod:`dotFsm` -- Generate a graph of a moddy finite state machine
 =======================================================================
 
@@ -6,7 +6,7 @@
    :synopsis: Generate a graph of a moddy finite state machine
 .. moduleauthor:: Klaus Popp <klauspopp@gmx.de>
 
-'''
+"""
 import subprocess
 import os
 
@@ -15,13 +15,13 @@ from .utils import create_dirs_and_open_output_file
 
 
 def gen_fsm_graph(fsm, file_name, keep_gv_file=False):
-    '''
+    """
     Generate a Fsm Graph of an fsm using the GraphViz dot tool
 
     :param fsm: an instance of the fsm
     :param file_name: the relative filename including '.svg'
     :param keep_gv_file: if True, don't delete graphviz input file
-    '''
+    """
     dot_fsm = DotFsm(fsm)
     dot_fsm.dot_gen(file_name, keep_gv_file)
 
@@ -32,50 +32,50 @@ def _space(indent):
 
 
 def map_state_names(name, state):
-    if state == '':
-        s = 'INIT'
+    if state == "":
+        s = "INIT"
     else:
         s = state
 
-    if name != '':
-        s = name + '_' + s
+    if name != "":
+        s = name + "_" + s
     return s
 
 
 class DotFsm(object):
-    '''
+    """
     Display the an fsm via the DOT language (Graphviz)
     States are vizualized as nodes
     Subfsms are drawn in separate subgraphs, an edge is drawn from the main state to the
     initial state in the subfsm
-    '''
+    """
 
     def __init__(self, fsm):
         self._fsm = fsm
 
-    def fsm_gen(self, level, fsm, name='', is_sub_fsm=False):
-        '''
+    def fsm_gen(self, level, fsm, name="", is_sub_fsm=False):
+        """
         generate the dot language statements for a (sub)fsm
         return lines,initial_state
         initial_state is only valid on subFsms
-        '''
+        """
         lines = []
         initial_state = None
 
         dict_transitions = fsm.get_dict_transitions()
         # States
         for state, list_trans in dict_transitions.items():
-            if state == '':
-                sstr = '%sstr [style=invisible];' % \
-                    map_state_names(name, state)
+            if state == "":
+                sstr = "%s [style=invisible];" % map_state_names(name, state)
                 if is_sub_fsm:
                     sstr = None
-            elif state == 'ANY':
-                sstr = '%sstr [label="from any state" shape=none];' % \
-                    map_state_names(name, state)
+            elif state == "ANY":
+                sstr = (
+                    '%s [label="from any state" shape=none];'
+                    % map_state_names(name, state)
+                )
             else:
-                sstr = '%sstr [label=%sstr];' % \
-                    (map_state_names(name, state), state)
+                sstr = "%s [label=%s];" % (map_state_names(name, state), state)
             if sstr is not None:
                 lines.append([level, sstr])
 
@@ -87,54 +87,80 @@ class DotFsm(object):
                     sub_fsm_name, cls = trans
                     # subfsm specification, instantiate subfsm
                     sub_fsm = sub_fsm_cls(parentFsm=fsm)
-                    lines.append([level, 'subgraph cluster_%s {  ' %
-                                  (sub_fsm_name)])
-                    lines.append([level + 1, 'label="%s";' %
-                                  (sub_fsm_name)])
+                    lines.append(
+                        [level, "subgraph cluster_%s {  " % (sub_fsm_name)]
+                    )
+                    lines.append([level + 1, 'label="%s";' % (sub_fsm_name)])
 
                     # draw subfsm
-                    sub_lines, sub_initial_state = \
-                        self.fsm_gen(level + 1, sub_fsm,
-                                     name=sub_fsm_name, is_sub_fsm=True)
+                    sub_lines, sub_initial_state = self.fsm_gen(
+                        level + 1, sub_fsm, name=sub_fsm_name, is_sub_fsm=True
+                    )
                     lines += sub_lines
-                    lines.append([level, '}' ])
+                    lines.append([level, "}"])
 
                     # draw edge from main state to subfsm initial state
-                    lines.append([level, '%s -> %s [color=lightgrey];' %
-                                  (map_state_names(name, from_state),
-                                   sub_initial_state)])
+                    lines.append(
+                        [
+                            level,
+                            "%s -> %s [color=lightgrey];"
+                            % (
+                                map_state_names(name, from_state),
+                                sub_initial_state,
+                            ),
+                        ]
+                    )
                 else:
                     # normal transition
                     event, to_state = trans
-                    if is_sub_fsm and event == 'INITIAL':
+                    if is_sub_fsm and event == "INITIAL":
                         initial_state = map_state_names(name, to_state)
                     else:
-                        lines.append([level, '%s -> %s [label="%s"];' %
-                                      (map_state_names(name, from_state),
-                                       map_state_names(name, to_state),
-                                       event)])
+                        lines.append(
+                            [
+                                level,
+                                '%s -> %s [label="%s"];'
+                                % (
+                                    map_state_names(name, from_state),
+                                    map_state_names(name, to_state),
+                                    event,
+                                ),
+                            ]
+                        )
         return lines, initial_state
 
     def dot_gen(self, file_name, keep_gv_file):
         level = 0
         lines = []
-        lines.append([level, 'digraph G {'])
-        lines.append([level + 1, 'rankdir=TB;'])
-        lines.append([level + 1,
-                      'graph [fontname = "helvetica" fontsize=10 '
-                      'fontnodesep=0.1];'])
-        lines.append([level + 1,
-                      'node [fontname = "helvetica" fontsize=10 '
-                      'shape=ellipse color=black height=.1];'])
-        lines.append([level + 1,
-                      'edge [fontname = "helvetica" color=black fontsize=8 '
-                      'fontcolor=black];'])
+        lines.append([level, "digraph G {"])
+        lines.append([level + 1, "rankdir=TB;"])
+        lines.append(
+            [
+                level + 1,
+                'graph [fontname = "helvetica" fontsize=10 '
+                "fontnodesep=0.1];",
+            ]
+        )
+        lines.append(
+            [
+                level + 1,
+                'node [fontname = "helvetica" fontsize=10 '
+                "shape=ellipse color=black height=.1];",
+            ]
+        )
+        lines.append(
+            [
+                level + 1,
+                'edge [fontname = "helvetica" color=black fontsize=8 '
+                "fontcolor=black];",
+            ]
+        )
 
         sub_lines, _ = self.fsm_gen(level + 1, self._fsm)
         lines += sub_lines
 
         # finish
-        lines.append([level, '}'])
+        lines.append([level, "}"])
 
         # Output the DOT file as filename.dot e.g. test.svg.gv
         dot_file = "%s.gv" % file_name
@@ -142,8 +168,7 @@ class DotFsm(object):
         for line in lines:
             file_desc.write("%s%s\n" % (_space(line[0]), line[1]))
         file_desc.close()
-        subprocess.check_call(['dot', '-Tsvg', dot_file, '-o%s' % file_name])
+        subprocess.check_call(["dot", "-Tsvg", dot_file, "-o%s" % file_name])
         print("Saved FSM graph to %s" % file_name)
         if not keep_gv_file:
             os.unlink(dot_file)
-
--- src/moddy/trace_to_csv.py
+++ src/moddy/trace_to_csv.py
@@ -1,4 +1,4 @@
-'''
+"""
 :mod:`trace_to_csv` -- Export simulator trace to csv
 =======================================================================
 
@@ -6,7 +6,7 @@
    :synopsis: Export simulator trace to csv
 .. moduleauthor:: Klaus Popp <klauspopp@gmx.de>
 
-'''
+"""
 
 import csv
 from .sim_base import time_unit_to_factor
@@ -14,7 +14,7 @@ from .utils import create_dirs_and_open_output_file
 
 
 def gen_trace_table(sim, file_name, **kwargs):
-    '''
+    """
     Moddy high level function to create trace tables as .csv.
 
     :param sim sim: Simulator instance
@@ -25,14 +25,14 @@ def gen_trace_table(sim, file_name, **kwargs):
      * timeUnit="s" - time unit for all time stamps in table
          ('s', 'ms', 'us', 'ns')
      * floatComma=',' - Comma character for float numbers
-    '''
+    """
     trc = TraceToCsv(sim.tracing.traced_events(), **kwargs)
     trc.save(file_name)
 
 
-class TraceToCsv():
+class TraceToCsv:
     # pylint: disable=too-few-public-methods
-    ''' class to generate csv trace table '''
+    """ class to generate csv trace table """
 
     def __init__(self, ev_list, time_unit="s", float_comma=","):
         self._ev_list = ev_list
@@ -42,32 +42,43 @@ class TraceToCsv():
 
     def _time_fmt(self, time):
         return ("%.6f" % (time / self._time_unit_factor)).replace(
-            ".", self._float_comma)
+            ".", self._float_comma
+        )
 
     def save(self, file_name):
-        ''' save the trace file '''
+        """ save the trace file """
         trace_file = create_dirs_and_open_output_file(file_name)
 
         csv.register_dialect(
-            'mydialect',
-            delimiter=';',
+            "mydialect",
+            delimiter=";",
             quotechar='"',
             doublequote=True,
             skipinitialspace=True,
-            lineterminator='\n',
-            quoting=csv.QUOTE_MINIMAL)
+            lineterminator="\n",
+            quoting=csv.QUOTE_MINIMAL,
+        )
 
-        writer = csv.writer(trace_file, dialect='mydialect')
+        writer = csv.writer(trace_file, dialect="mydialect")
 
         # Write Comment row
-        row = ['#time', 'Action', 'Object', 'Port/Tmr',
-               'Value', 'requestTime', 'startTime', 'endTime', 'flightTime']
+        row = [
+            "#time",
+            "Action",
+            "Object",
+            "Port/Tmr",
+            "Value",
+            "requestTime",
+            "startTime",
+            "endTime",
+            "flightTime",
+        ]
         writer.writerow(row)
 
         for trace_ev in self._ev_list:
             row = [self._time_fmt(trace_ev.trace_time), trace_ev.action]
             if trace_ev.part is None:
-                part = 'Global'
+                part = "Global"
             else:
                 part = trace_ev.part.hierarchy_name()
             row.append(part)
@@ -75,26 +86,32 @@ class TraceToCsv():
             if trace_ev.sub_obj is not None:
                 row.append(trace_ev.sub_obj.hierarchy_name_with_type())
             else:
-                row.append('')
+                row.append("")
             if trace_ev.trans_val is not None:
-                if trace_ev.action.find('MSG') != -1:
+                if trace_ev.action.find("MSG") != -1:
                     # print request, begin, end, flightTime and
                     # msg in separate columns
                     fire_event = trace_ev.trans_val
-                    row.append("(***LOST***)" if fire_event.is_lost else
-                               fire_event.msg_text())
+                    row.append(
+                        "(***LOST***)"
+                        if fire_event.is_lost
+                        else fire_event.msg_text()
+                    )
                     row.append(self._time_fmt(fire_event.request_time))
-                    row.append(self._time_fmt(fire_event.exec_time -
-                                              fire_event.flight_time))
+                    row.append(
+                        self._time_fmt(
+                            fire_event.exec_time - fire_event.flight_time
+                        )
+                    )
                     row.append(self._time_fmt(fire_event.exec_time))
                     row.append(self._time_fmt(fire_event.flight_time))
-                elif trace_ev.action.find('T-') != -1:
+                elif trace_ev.action.find("T-") != -1:
                     timeout_fmt = trace_ev.trans_val
                     row.append(self._time_fmt(timeout_fmt.timeout))
                 else:
                     row.append(trace_ev.trans_val.__str__())
             else:
-                row.append('')
+                row.append("")
 
             # print("ROW=", row)
 
