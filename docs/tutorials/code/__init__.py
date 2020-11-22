8dfeb37b1062515ec1f2ce19b57054bccea6a981
popples <klauspopp@gmx.de>
2020-11-22 14:31:10 +0100

    fix tutorials code init.py

---
+++ docs/tutorials/code/__init__.py
@@ -0,0 +1,11 @@
+# run all tutorials
+
+import os
+
+if __name__ == "__main__":
+    files = os.listdir(".")
+
+    for file in files:
+        if file.endswith(".py") and file != "__init__.py":
+            print("================ execute %s ====================" % file)
+            os.system(file)
